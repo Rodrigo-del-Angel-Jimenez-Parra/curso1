d3b0166ff280c4cdfbad227fd490a9afcec54b8e
RodrigodelAngel JimenezParra <ingeniero_rodrigo_del_angel_jimenez_parra@outlook.com>
2022-05-20 15:44:26 -0500

    Modifique el archivo codigo1.cpp y ahora pide 3 numeros y los suma

--- c++/codigo1.cpp
+++ c++/codigo1.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 int main()
 {
-    int n, n2, r;
-    cout<< "Hola ingresa dos numeros para sumar\n";
-    cin>> n >>n2;
+    int n, n2, n3, r;
+    cout<< "Hola ingresa 3 numeros para sumar\n";
+    cin>> n >>n2 >>n3;
 
-    r=n+n2;
+    r=n+n2+n3;
     cout<< "El resultado es:\n" <<r;
     return 0;
 }
