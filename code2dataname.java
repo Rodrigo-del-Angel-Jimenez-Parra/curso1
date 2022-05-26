974e384134e8278d3e1434a34a0e53a017516396
RodrigodelAngel JimenezParra <ingeniero_rodrigo_del_angel_jimenez_parra@outlook.com>
2022-05-26 18:49:57 -0500

    Agregue code 2 el cual muestra los tipos de datos

---
+++ code2dataname.java
@@ -0,0 +1,21 @@
+public class code2dataname 
+{
+    public static void main(String[] args) 
+    {
+        byte a;
+        a=127;
+        System.out.println("byte= "+a);
+        short b;
+        b=32767;
+        System.out.println("short= "+b); 
+        int c;
+        c=2147483647;
+        System.out.println("int= "+c);
+        char d;
+        d= 'c';
+        System.out.println("char= "+d);
+        boolean e;
+        e=true;
+        System.out.println("boolean= "+e);
+    }
+}
