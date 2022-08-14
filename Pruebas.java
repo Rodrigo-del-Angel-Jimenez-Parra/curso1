e139ae6f599aeb5f9c9975e848985ff324523fdb
RodrigodelAngel JimenezParra <ingeniero_rodrigo_del_angel_jimenez_parra@outlook.com>
2022-08-14 16:23:59 -0500

    Actualize el archivo de pruebas

--- Pruebas.java
+++ Pruebas.java
@@ -4,6 +4,27 @@ public class Pruebas
 {
     public static void main(String[] args) 
     {
-        
-    }    
+        Scanner e = new Scanner(System.in);
+
+        int n=10;
+        String x;
+        x= Integer.toString(n);
+        System.out.println(x);
+        /* 
+        int n, c=0;
+        int[] vn= new int[9];
+
+        for (int i = 0; i <= vn.length; i++) 
+        {
+            System.out.print("Ingrese un numero: ");
+            vn[i]= e.nextInt();
+            if (n==0) 
+            {
+                c++;
+            }
+        }
+        System.out.println("Los numeros que ingreso fueron: " +vn[1]);
+        System.out.println("La cantidad de 0:" +c);
+        */
+    }
 }
